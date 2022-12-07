031d8b4b7afd1c67776301e6557106523cecd126
Luis Sales <luissales2608@gmail.com>
2022-12-07 20:25:53 -0300

    adicionando rotas

--- SRC/routes/routes.js
+++ SRC/routes/routes.js
@@ -3,5 +3,7 @@ const ProductController = require('../controllers/products')
 
 router.get('/products', ProductController.get)
 router.post('/products', ProductController.post)
+router.put('/products/:id', ProductController.put)
+router.delete('/products/:id', ProductController.remove)
 
 module.exports= router
